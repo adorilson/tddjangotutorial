72667eccebb2e8817cbf57095e2966f538e8e347
Adorilson Bezerra <adorilson@gmail.com>
2012-11-03 22:49:33 -0300

    - Seção 'Writing the FT as comments' e 'Setting up data for the test via the admin site da Part' 3

--- fts/tests.py
+++ fts/tests.py
@@ -1,6 +1,27 @@
+from collections import namedtuple
+
 from django.test import LiveServerTestCase
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+PollInfo = namedtuple('PollInfo', ['question', 'choices'])
+POLL1 = PollInfo(
+    question = "How awesome is Test-Driven Development?",
+    choices = [
+        'Very awesome',
+        'Quite awesome',
+        'Moderately awesome',
+    ],
+)
+POLL2 = PollInfo(
+    question = 'Which workshop treat do you prefer?',
+    choices = [
+        'Beer',
+        'Pizza',
+        'The Acquisition of Knowledge',
+    ],
+)
 
 class PollsTest(LiveServerTestCase):
     fixtures = ['admin_user.json']
@@ -36,7 +57,7 @@ class PollsTest(LiveServerTestCase):
         # She now sees a couple of hyperlink that says "Polls"
         polls_links = self.browser.find_elements_by_link_text('Polls')
         self.assertEquals(len(polls_links), 2)
-        
+
         # The second one looks more exciting, so she clicks it
         polls_links[1].click()
         #self.browser.get(self.live_server_url +  '/admin/polls/poll/')
@@ -59,7 +80,7 @@ class PollsTest(LiveServerTestCase):
         # She types in an interesting question for the Poll
         question_field = self.browser.find_element_by_name('question')
         question_field.send_keys(u'How awesome is Test-Driven Development?')
-       
+
         # She sets the date and time of publication - it'll be a new year's poll
         date_field = self.browser.find_element_by_name('pub_date_0')
         date_field.send_keys('01/01/12')
@@ -85,5 +106,78 @@ class PollsTest(LiveServerTestCase):
                        "How awesome is Test-Driven Development?"
                        )
         self.assertEquals(len(new_poll_links), 1)
-        
+
         # Satisfied, she goes back to sleep
+
+    def _setup_polls_via_admin(self):
+
+        # Gertrude logs into the admin site
+        self.browser.get(self.live_server_url + '/admin/')
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('adm1n')
+        password_field.send_keys(Keys.RETURN)
+
+        # She has a number of polls to enter . For each one, she:
+        for poll_info in [POLL1, POLL2]:
+            #Follows the link to Polls app, and adds a new Poll
+            self.browser.find_elements_by_link_text('Polls')[1].click()
+            self.browser.find_element_by_link_text('Add poll').click()
+
+            # Enter its name, and uses the 'today' and 'now' buttons to set
+            # the publish date
+            question_field = self.browser.find_element_by_name('question')
+            question_field.send_keys(poll_info.question)
+            self.browser.find_element_by_link_text('Today').click()
+            self.browser.find_element_by_link_text('Now').click()
+
+            # Sees she can entre choices for the Poll on this same page,
+            # so she does
+            for i, choice_text in enumerate(poll_info.choices):
+                choice_field = self.browser.find_element_by_name('choice_set-%d-choice' % i)
+                choice_field.send_keys(choice_text)
+
+            # Saves her new poll
+            save_button = self.browser.find_element_by_css_selector("input[value='Save']")
+            save_button.click()
+
+            # Is returned to the "Polls" listing, where she can her
+            # new poll, listed as a clickable link by its name
+            new_poll_links = self.browser.find_elements_by_link_text(
+                poll_info.question
+            )
+            self.assertEquals(len(new_poll_links), 1)
+
+            self.browser.get(self.live_server_url + '/admin/')
+
+        # She logs out of the admin site
+        self.browser.find_element_by_link_text('Log out').click()
+
+    def test_voting_on_a_new_poll(self):
+        # First, Gertrude the administrator logs into admin site and
+        # creates a couple of news Polls, and theis response choice_1
+        self._setup_polls_via_admin()
+        self.fail('TODO')
+        # Now, Herbert the regular user goes to homepage of the site. He
+        # sees a list of polls.
+
+        # He clicks on the link to the first poll, which is called
+        # 'How awesome is Test-Driven Development?'
+
+        # He is taken to a poll 'results' page, which says
+        # "no-one has voted on this poll yet"
+
+        # He also sees a form, which offers him several choices.
+        # He decided to select "very awesome"
+
+        # He clicks 'submit'
+
+        # The page refreshes, and he sees that his choice
+        # has updated the results. They now say
+        # "100% : very awesome".
+
+        # The page also says "1 votes"
+
+        # Satisfied, he goes back to sleep
+
